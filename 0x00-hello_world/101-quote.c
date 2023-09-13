caa4df8bd413368763aa3366d9b553c18a64695b
Eric <ericsamueljnr@gmail.com>
2023-09-13 11:01:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
  * main - Entry point
- * Return: Always 1 (Success)
- */
+ * @void: parameter
+ * Return: 1 (success)
+ **/
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+    fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+    return (1);
 }
